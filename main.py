351b19558150313fca5b13250f72193b18307201
Abylai <aby.beken@aues.kz>
2021-12-24 00:21:44 +0600

    snake blok, speed kosyldi

--- main.py
+++ main.py
@@ -14,3 +14,13 @@ blue = (50, 153, 213)
 dis_width = 600
 dis_height = 400
  
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game')
+ 
+clock = pygame.time.Clock()
+ 
+snake_block = 10
+snake_speed = 15
+ 
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
